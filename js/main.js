3c49beadf1416f8711508878bf900bb0fbff2086
John Tendik <johntendik@hotmail.com>
2016-08-17 13:46:52 -0400

    removed animation for port section
    removed menu

--- js/main.js
+++ js/main.js
@@ -1,53 +1,3 @@
-//Menu vars
-var mainMenu = document.getElementById("main_nav"),
-	responsiveMenuBtn = document.getElementById("responsiveMenuBtn");
-
-// Event Listener for the menu
-if(window.attachEvent) {
-    window.attachEvent('onresize', function(event1) {
-        updateMenu(event1);
-    });
-}
-else if(window.addEventListener) {
-    window.addEventListener('resize', function(event1) {
-        updateMenu(event1);
-    }, true);
-}
-else {
-    alert("Please update your web browser to the current standards.");
-}
-
-function updateMenu(eve){	
-	var wWidth = window.innerWidth;
-	
-	if(wWidth < 666){
-		mainMenu.className = "responsiveNavStyles";
-	}else{
-		mainMenu.className = "";
-	}
-}
-
-updateMenu();
-
-// Event Listener for the menu btn
-if(window.attachEvent) {
-    responsiveMenuBtn.attachEvent('click', function(event1) {
-        
-    });
-}
-else if(window.addEventListener) {
-    responsiveMenuBtn.addEventListener('click', function(event1) {
-        if(mainMenu.classList.contains("shown")){
-        	mainMenu.className = "responsiveNavStyles notshown";
-        }
-        else{
-        	mainMenu.className = "responsiveNavStyles shown";
-        }
-    }, true);
-}
-else {
-    alert("Please update your web browser to the current standards.");
-}
 // Portfolio stuff and thangs
 
 var portList = document.getElementsByClassName("portItem"),
@@ -57,8 +7,6 @@ var portList = document.getElementsByClassName("portItem"),
 	portImageItem = document.getElementById("portItemImage");
 	nextPortBtn = document.getElementsByClassName("next"),
 	prevPortBtn = document.getElementsByClassName("prev");
-
-console.log(nextPortBtn);
 
 function nextPortItem(){
 	currentPort++;
@@ -88,37 +36,4 @@ function animatePortImage(){
 		portImageItem.src = "img/port"+currentPort+".png";
 		portImageItem.className = "slideInImg";
 	},300);
-}
-
-
-var aboutSection = document.getElementById("about"),
-	portSection = document.getElementById("portfolio"),
-	contactSection = document.getElementById("contact");
-
-function posTop() {
-    return typeof window.pageYOffset != 'undefined' ? window.pageYOffset: document.documentElement.scrollTop? document.documentElement.scrollTop: document.body.scrollTop? document.body.scrollTop:0;
-}
-
-// Event Listener for the fades
-if(window.attachEvent) {
-    window.attachEvent('scroll', function(event1) {
-        fadeInOutSection(event1);
-    });
-}
-else if(window.addEventListener) {
-    window.addEventListener('scroll', function(event1) {
-        fadeInOutSection(event1);
-    }, false);
-}
-else {
-    alert("Please update your web browser to the current standards.");
-}
-
-function fadeInOutSection(evnt){
-
-	if(posTop() > portSection.offsetTop / 2)
-		portSection.className = "fadeIn";
-	else
-		portSection.className = "";
-
 }
